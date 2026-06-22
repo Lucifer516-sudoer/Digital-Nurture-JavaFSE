3d2f59d2870c5a8b72bcf6b5bc53594dfa8d5428
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-06-22 14:34:51 +0530

    refactor: Rearrange constructor and field declaration in SingletonPattern class

--- Week1/day_17_06_2026/SingletonPattern.java
+++ Week1/day_17_06_2026/SingletonPattern.java
@@ -3,14 +3,6 @@ public class SingletonPattern {
     // Referred: refactoring.guru
     //
     private static SingletonPattern instance;
-    public String someValue; // I actually don't need this for this example, but i am including it
-
-    // Constructor
-    // I am really confused about access modifiers, and then need to learn about a
-    // lot, to complete these 13 modules
-    SingletonPattern(String value) {
-        this.someValue = value;
-    }
 
     public static SingletonPattern getInstance(String value) {
         if (instance == null) {
@@ -22,5 +14,14 @@ public class SingletonPattern {
 
     public static void main(String[] args) {
 
+    }
+
+    public String someValue; // I actually don't need this for this example, but i am including it
+
+    // Constructor
+    // I am really confused about access modifiers, and then need to learn about a
+    // lot, to complete these 13 modules
+    SingletonPattern(String value) {
+        this.someValue = value;
     }
 }
