a469a92cb1866e43f99969cc8b8397786a7af985
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-06-22 14:35:03 +0530

    feat: Implement product management features in EcommerceSearch and enhance Product class

---
+++ Week2/day_22_06_2026/EcommercePlatformSearch/Main.java
@@ -0,0 +1,145 @@
+package Week2.day_22_06_2026.EcommercePlatformSearch;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Going to use dummy prods
+        Product[] prods = {
+
+                new Product(
+                        "iPhone 15",
+                        69999,
+                        10,
+                        25,
+                        "Apple Store",
+                        Rating.STAR_5,
+                        Category.ELECTRONICS),
+                new Product(
+                        "iPhone 15",
+                        68499,
+                        12,
+                        18,
+                        "Reliance Digital",
+                        Rating.STAR_5,
+                        Category.ELECTRONICS),
+                new Product(
+                        "Samsung Galaxy S24",
+                        62999,
+                        15,
+                        30,
+                        "Samsung India",
+                        Rating.STAR_5,
+                        Category.ELECTRONICS),
+                new Product(
+                        "Boat Airdopes 311",
+                        1499,
+                        45,
+                        250,
+                        "Boat Lifestyle",
+                        Rating.STAR_4,
+                        Category.ELECTRONICS),
+                new Product(
+                        "Sony WH-1000XM5",
+                        24999,
+                        20,
+                        14,
+                        "Sony",
+                        Rating.STAR_5,
+                        Category.ELECTRONICS),
+                new Product(
+                        "Puma Running Shoes",
+                        3299,
+                        35,
+                        72,
+                        "Puma",
+                        Rating.STAR_4,
+                        Category.FASHION),
+                new Product(
+                        "Levi's Slim Fit Jeans",
+                        2299,
+                        40,
+                        95,
+                        "Levi's",
+                        Rating.STAR_4,
+                        Category.FASHION),
+                new Product(
+                        "Prestige Pressure Cooker",
+                        1899,
+                        30,
+                        120,
+                        "Prestige",
+                        Rating.STAR_5,
+                        Category.HOME_AND_KITCHEN),
+                new Product(
+                        "Milton Water Bottle",
+                        499,
+                        18,
+                        500,
+                        "Milton",
+                        Rating.STAR_4,
+                        Category.HOME_AND_KITCHEN),
+                new Product(
+                        "Mamaearth Face Wash",
+                        299,
+                        22,
+                        340,
+                        "Mamaearth",
+                        Rating.STAR_4,
+                        Category.BEAUTY_AND_PERSONAL_CARE),
+                new Product(
+                        "Dove Shampoo",
+                        449,
+                        15,
+                        200,
+                        "Hindustan Unilever",
+                        Rating.STAR_5,
+                        Category.BEAUTY_AND_PERSONAL_CARE),
+                new Product(
+                        "Aashirvaad Atta 10kg",
+                        529,
+                        8,
+                        150,
+                        "ITC",
+                        Rating.STAR_5,
+                        Category.GROCERY),
+                new Product(
+                        "Tata Salt 1kg",
+                        30,
+                        5,
+                        800,
+                        "Tata Consumer",
+                        Rating.STAR_5,
+                        Category.GROCERY),
+                new Product(
+                        "Amul Butter 500g",
+                        285,
+                        12,
+                        90,
+                        "Amul",
+                        Rating.STAR_4,
+                        Category.GROCERY),
+                new Product(
+                        "Chetak",
+                        1900,
+                        35,
+                        6023,
+                        "Hindisthan Enterprises",
+                        Rating.STAR_4,
+                        Category.ELECTRONICS)
+        };
+
+        EcommerceSearch searcher = new EcommerceSearch();
+        for (Product each : prods) {
+            searcher.addNewProduct(each);
+
+        }
+        System.out.println("======================================");
+        System.out.println("All Products");
+        System.out.println("======================================");
+        searcher.printProducts(); // See too many responsibilities
+        System.out.println("Searching by Category ...");
+        List<Product> res = searcher.filterByPrice(200, 500);
+        searcher.printProducts(res);
+    }
+}
--- Week2/day_22_06_2026/EcommercePlatformSearch/Product.java
+++ Week2/day_22_06_2026/EcommercePlatformSearch/Product.java
@@ -3,17 +3,86 @@ package Week2.day_22_06_2026.EcommercePlatformSearch;
 import java.util.ArrayList;
 
 public class Product {
-    String name;
+    private final String name;
     float price;
     float discount;
+    long stocksLeft;
+    private final String seller;
     Rating rating;
-    Category category;
-    ArrayList<Review> reviews;
+    private final Category category;
+    private ArrayList<Review> reviews;
+
+    public Product(String name, float price, float discount, long stocksLeft, String seller, Rating rating,
+            Category category) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.stocksLeft = stocksLeft;
+        this.seller = seller;
+        this.rating = rating;
+        this.category = category;
+    }
+
+    public String getIdentifier() {
+        return this.name + this.seller;
+    }
 
     @Override
     public String toString() {
-        return "Product [name=" + name + ", price=" + price + ", discount=" + discount + ", rating=" + rating
-                + ", category=" + category + ", totalreviews=" + reviews.size() + "]";
+        return "Product [name=" + name + ", price=" + price + ", discount=" + discount + ", stocksLeft=" + stocksLeft
+                + ", seller=" + seller + ", rating=" + rating + ", category=" + category + "]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(final float price) {
+        this.price = price;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(final float discount) {
+        this.discount = discount;
+    }
+
+    public long getStocksLeft() {
+        return stocksLeft;
+    }
+
+    public void setStocksLeft(final long stocksLeft) {
+        this.stocksLeft = stocksLeft;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(final Rating rating) {
+        this.rating = rating;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public ArrayList<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(final ArrayList<Review> reviews) {
+        this.reviews = reviews;
     }
 
 }
