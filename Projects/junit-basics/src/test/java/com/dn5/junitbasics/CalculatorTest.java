8449fe1d011dc8a349d5fcc023c1bce363f9e1fd
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-06-24 11:58:31 +0530

    feat: Implement Calculator class with basic arithmetic operations and corresponding tests

---
+++ Projects/junit-basics/src/test/java/com/dn5/junitbasics/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.dn5.junitbasics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private final Calculator calc = new Calculator(); // Arrange (here I made it as a `fixture`)
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3)); // Act & Assert
+        /// AAA in action
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(-1, calc.subtract(2, 3));
+
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calc.multiply(2, 3));
+
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(0, calc.divide(2, 3));
+        assertThrows(
+                ArithmeticException.class,
+                () -> calc.divide(10, 0));
+
+    }
+
+}
