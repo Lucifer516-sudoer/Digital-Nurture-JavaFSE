711eab528909015b65c202fbe0f7582c22ae0ec8
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-07-02 11:04:35 +0530

    fix: Update BankServiceMockitoTestVerify to use anyInt() for account balance retrieval and adjust verification counts

--- Projects/mockito-basics/src/test/java/com/dn5/BankServiceMockitoTestVerify.java
+++ Projects/mockito-basics/src/test/java/com/dn5/BankServiceMockitoTestVerify.java
@@ -1,7 +1,9 @@
 package com.dn5;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -23,22 +25,24 @@ public class BankServiceMockitoTestVerify {
     @Test
     void testIfWithdrawalCallWasMade() {
         // arranging Stub(s)
-        when(database.getAccountBalance(1)).thenReturn(5000.0f);
+        when(database.getAccountBalance(anyInt())).thenReturn(5000.0f);
+        // anyInt() seems to allow the mock object to take in any integer value and here
+        // outputs 5000f alone
 
         // act & assert
         assertTrue(bankService.canWithdraw(1, 500));
-        assertTrue(bankService.canWithdraw(1, 500));
+        assertTrue(bankService.canWithdraw(100, 500));
 
         // Verify
-        verify(database, times(2)).getAccountBalance(1);
+        verify(database, times(1)).getAccountBalance(1);
+        verify(database, times(1)).getAccountBalance(100);
         /// here the verify method is acting as a proxy and getting wt we need
     }
 
-    @AfterEach
-    void tearDown() {
-        // Not sure, whether this is needed or not ...
-        // Since ther is garbage collection, but still the question is, "Is it needed?"
-        database = null;
-        bankService = null;
+    @Test
+    void shouldNeverCall() {
+        verify(database, never()).getAccountBalance(1);
     }
+
+    // No need for tear down, auto GC and Mockito takes care
 }
