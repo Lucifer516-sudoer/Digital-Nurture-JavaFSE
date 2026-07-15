d14207302d182828675920e06fcd8f173fd95970
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-07-15 11:23:37 +0530

    feat: update POM and App class to include SLF4J logging dependencies and implement logging functionality

--- Projects/sl4j-basics-handson/src/main/java/com/dn5/App.java
+++ Projects/sl4j-basics-handson/src/main/java/com/dn5/App.java
@@ -1,13 +1,18 @@
 package com.dn5;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.error("The first exercise is stating me to use error first so yeah, Hello from error");
+        // now warning
+        logger.warn("Hey this is warning, I am gonna sleep ...");
     }
 }
