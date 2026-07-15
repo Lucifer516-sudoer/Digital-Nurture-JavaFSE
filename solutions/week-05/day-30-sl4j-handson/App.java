b8eec627340de50bdfde98088f9a49829d63fda5
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-07-15 11:23:47 +0530

    feat: add initial implementation of App class with SLF4J logging and README documentation

---
+++ solutions/week-05/day-30-sl4j-handson/App.java
@@ -0,0 +1,20 @@
+// package com.dn5;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.error("The first exercise is stating me to use error first so yeah, Hello from error");
+        // now warning
+        logger.warn("Hey this is warning, I am gonna sleep ...");
+    }
+}{
+
+}
---
+++ solutions/week-05/day-30-sl4j-handson/README.md
@@ -0,0 +1,4 @@
+# 15th July, 2026 - 11:21:06 AM
+
+- It has been a while, and I don't know whether I will complete this or not, but i will try my best
+- Refer to the [sl4j-basics-handson](../../../Projects/sl4j-basics-handson), coz, that is where I actually wrote this
