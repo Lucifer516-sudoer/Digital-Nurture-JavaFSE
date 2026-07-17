9dc9f99a7f703239921b1571b23d033aec855e2e
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-07-17 14:15:58 +0530

    log: capture + solution lombok-tostring (week-05 day-32)

---
+++ solutions/week-05/day-32-lombok-tostring/Student.java
@@ -0,0 +1,14 @@
+package com.dn5;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class Student {
+    private int id;
+    private String name;
+    private int yearOfBirth;
+}
