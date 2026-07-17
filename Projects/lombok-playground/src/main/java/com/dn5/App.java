adc510567a8ffa7a96f195190cf0e7e9cc3c03b2
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-07-17 15:37:56 +0530

    refactor: update Student class to use final id and RequiredArgsConstructor; simplify App class instantiation

--- Projects/lombok-playground/src/main/java/com/dn5/App.java
+++ Projects/lombok-playground/src/main/java/com/dn5/App.java
@@ -14,30 +14,12 @@ public class App {
     }
 
     public static void main(String[] args) {
-        Student student = new Student();
         // created three students in here itself
-        Student student1 = new Student();
-        Student student2 = new Student();
-        Student student3 = new Student();
+        Student student1 = new Student(1);
+        Student student2 = new Student(2);
 
-        student1.setId(1);
-        student1.setName("Alwin");
-        student1.setYearOfBirth(2005);
-
-        student2.setId(1);
-        student2.setName("Alwin");
-        student2.setYearOfBirth(2005);
-
-        student3.setId(1);
-        student3.setName("Emily"); // Sweet name, isnt it?
-        student3.setYearOfBirth(2005);
-
-        print("Student 1: " + student1.toString());
-        print("Student 2: " + student2.toString());
-        print("Student 3: " + student3.toString());
-        print("S1 == S2 :: " + student1.equals(student2));
-        print("S2 == S1 :: " + student2.equals(student1));
-        print("S1 == S3 :: " + student1.equals(student3));
+        print(student1.toString());
+        print(student2.toString());
 
     }
 }
--- Projects/lombok-playground/src/main/java/com/dn5/Student.java
+++ Projects/lombok-playground/src/main/java/com/dn5/Student.java
@@ -2,15 +2,18 @@ package com.dn5;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@RequiredArgsConstructor
 @EqualsAndHashCode
 @ToString
 @Setter
 @Getter
 public class Student {
-    private int id;
+    @EqualsAndHashCode.Exclude
+    private final int id;
     private String name;
     private int yearOfBirth;
 }
