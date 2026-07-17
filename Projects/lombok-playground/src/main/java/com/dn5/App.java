e4333e29d7562573ec63c3a6b7dbbb6a04c82b21
Lucifer516-sudoer <harishvengatachalam02092005@gmail.com>
2026-07-17 15:27:30 +0530

    feat: update Student class with equals and hashcode annotations, and modify App class to demonstrate usage

--- Projects/lombok-playground/src/main/java/com/dn5/App.java
+++ Projects/lombok-playground/src/main/java/com/dn5/App.java
@@ -15,12 +15,29 @@ public class App {
 
     public static void main(String[] args) {
         Student student = new Student();
-        // Just removed the gettters; don't need it now.
-        // and also dont need to print getters
-        student.setId(1);
-        student.setName("Emily");
-        student.setYearOfBirth(2005);
-        print("Printing the whole student (I suppose I can call this dataclass for my sake...)");
-        print(student.toString());
+        // created three students in here itself
+        Student student1 = new Student();
+        Student student2 = new Student();
+        Student student3 = new Student();
+
+        student1.setId(1);
+        student1.setName("Alwin");
+        student1.setYearOfBirth(2005);
+
+        student2.setId(1);
+        student2.setName("Alwin");
+        student2.setYearOfBirth(2005);
+
+        student3.setId(1);
+        student3.setName("Emily"); // Sweet name, isnt it?
+        student3.setYearOfBirth(2005);
+
+        print("Student 1: " + student1.toString());
+        print("Student 2: " + student2.toString());
+        print("Student 3: " + student3.toString());
+        print("S1 == S2 :: " + student1.equals(student2));
+        print("S2 == S1 :: " + student2.equals(student1));
+        print("S1 == S3 :: " + student1.equals(student3));
+
     }
 }
